865414787ed59504893ac8b9fc7e28d6e2f03d3e
sumaya-jahan <sumayajahan765@gmail.com>
2026-06-29 22:20:37 +0600

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-10 text-blue-600">
+        ReSell Hub
+      </h1>
+    </div>
+  );
+}
+
+export default App;
